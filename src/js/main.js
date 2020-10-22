9d1bd795a3e7920b1cc692402d62e0be45e5b1f7
Soobin Choi <soobinchoi@MacBook-Pro.local>
2020-10-22 03:30:13 -0700

    toggle layer

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,45 @@
 require([
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/layers/FeatureLayer",
-    "esri/widgets/BasemapToggle",
-    "esri/widgets/BasemapGallery"
-  ], function(Map, MapView, FeatureLayer, BasemapToggle, BasemapGallery) {
-    var map = new Map({
-      basemap: "topo-vector"
-    });
-
-    var view = new MapView({
-      container: "viewDiv",
-      map: map,
-      center: [-118.805, 34.027], // longitude, latitude
-      zoom: 13
-    });
-
-    var trailheadsLayer = new FeatureLayer({
-        url:
-          "https://services6.arcgis.com/vhDSkImOgoRe9GRT/arcgis/rest/services/MACMODS/FeatureServer/0"
-    });
-    // add layer      
-    map.add(trailheadsLayer);
-
-    // Trails feature layer (lines)
-    var trailsLayer = new FeatureLayer({
-    url:
-      "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
-    });
-    // add layer
-    map.add(trailsLayer, 0);
-  
-    // Parks and open spaces (polygons)
-    var parksLayer = new FeatureLayer({
-    url:
-      "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
-    });
-    // add layer
-    map.add(parksLayer, 0);
-
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer",
+], function (Map, MapView, FeatureLayer) {
+  var map = new Map({
+    basemap: "topo-vector",
   });
+
+  var view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-118.805, 34.027], // longitude, latitude
+    zoom: 13,
+  });
+
+  // Trails feature layer (lines)
+  var trailsLayer = new FeatureLayer({
+    url:
+      "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
+  });
+  // add layer
+  map.add(trailsLayer, 0);
+
+  // Parks and open spaces (polygons)
+  var parksLayer = new FeatureLayer({
+    url:
+      "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0",
+    visible: false,
+  });
+  // add layer
+  map.add(parksLayer, 0);
+
+  var parksLayerToggle = document.getElementById("streetsLayer");
+
+  /*****************************************************************
+   * The visible property on the layer can be used to toggle the
+   * layer's visibility in the view. When the visibility is turned off
+   * the layer is still part of the map, which means you can access
+   * its properties and perform analysis even though it isn't visible.
+   *******************************************************************/
+  parksLayerToggle.addEventListener("change", function () {
+    parksLayer.visible = parksLayerToggle.checked;
+  });
+});
