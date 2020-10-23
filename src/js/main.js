d534a24682de594ac534d28f618cb1168eb0e73e
Danny7226 <dchai2@uci.edu>
2020-10-23 13:33:33 -0700

    minor revise

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@ require([
   "esri/views/MapView",
   "esri/layers/FeatureLayer",
 ], function (Map, MapView, FeatureLayer) {
-  var map = new Map({
+  const map = new Map({
     basemap: "topo-vector",
   });
 
-  var view = new MapView({
+  const view = new MapView({
     container: "viewDiv",
     map: map,
     center: [-118.805, 34.027], // longitude, latitude
@@ -15,7 +15,7 @@ require([
   });
 
   // Trails feature layer (lines)
-  var trailsLayer = new FeatureLayer({
+  const trailsLayer = new FeatureLayer({
     url:
       "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
       visible: false,
@@ -33,8 +33,8 @@ require([
   // add layer
   map.add(parksLayer, 0);
 
-  // Data Layer 1 
-  var parksLayerToggle = document.getElementById("streetsLayer");
+  // Toggle function of Layer 1 
+  const parksLayerToggle = document.getElementById("streetsLayer");
 
   /*****************************************************************
    * The visible property on the layer can be used to toggle the
@@ -46,8 +46,8 @@ require([
     parksLayer.visible = parksLayerToggle.checked;
   });
 
-  // Data Layer 2
-  var trailsLayerToggle = document.getElementById("trailsLayer");
+  // Toggle function of Layer 2
+  const trailsLayerToggle = document.getElementById("trailsLayer");
 
   trailsLayerToggle.addEventListener("change", function () {
     trailsLayer.visible = trailsLayerToggle.checked;
