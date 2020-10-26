dca8b1014d212950ae113ef49cd498d36cb73696
oscarli9 <guowel2@uci.edu>
2020-10-26 22:37:02 +0800

    Added federal and state waters layer

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-//import { kelpProductivityLayerUrl, shippingLanesLayerUrl } from './config.js';
+//import { kelpProductivityLayerUrl, shippingLanesLayerUrl, dangerZonesAndRestrictedAreasLayerUrl, mpaInventoryLayerUrl } from './config.js';
 
 require([
   // mapping
@@ -15,9 +15,45 @@ require([
   // widgets
   Legend
 ) {
+  // Display kelp productivity with the heat map mode
+  const renderer = {
+    type: "heatmap",
+    colorStops: [
+      { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+      { color: "#472b77", ratio: 0.083 },
+      { color: "#4e2d87", ratio: 0.166 },
+      { color: "#563098", ratio: 0.249 },
+      { color: "#5d32a8", ratio: 0.332 },
+      { color: "#6735be", ratio: 0.415 },
+      { color: "#7139d4", ratio: 0.498 },
+      { color: "#7b3ce9", ratio: 0.581 },
+      { color: "#853fff", ratio: 0.664 },
+      { color: "#a46fbf", ratio: 0.747 },
+      { color: "#c29f80", ratio: 0.83 },
+      { color: "#e0cf40", ratio: 0.913 },
+      { color: "#ffff00", ratio: 1 }
+    ],
+    maxPixelIntensity: 10,
+    minPixelIntensity: 0
+  };
   // Kelp productivity layer
   const kelpProductivityLayer = new FeatureLayer({
     url: "https://services7.arcgis.com/4c8njmg1eMIbzYXM/arcgis/rest/services/ussw11999_maxcanopy/FeatureServer/0",
+    visible: false,
+    renderer: renderer
+  });
+
+  /*
+  // Bathymetry layer
+  const bathymetryLayer = new FeatureLayer({
+    url: "https://oceans2.arcgis.com/arcgis/rest/services/Seafloor_Bathymetry/ImageServer",
+    visible: false
+  });
+  */
+
+  // Federal and state waters layer
+  const federalAndStateWatersLayer = new FeatureLayer({
+    url: "https://services7.arcgis.com/4c8njmg1eMIbzYXM/ArcGIS/rest/services/FederalAndStateWaters/FeatureServer/0",
     visible: false
   });
 
@@ -41,7 +77,7 @@ require([
 
   const map = new Map({
     basemap: "topo-vector",
-    layers: [kelpProductivityLayer, shippingLanesLayer, dangerZonesAndRestrictedAreasLayer, mpaInventoryLayer]
+    layers: [kelpProductivityLayer, /*bathymetryLayer,*/ federalAndStateWatersLayer, shippingLanesLayer, dangerZonesAndRestrictedAreasLayer, mpaInventoryLayer]
   });
 
   const view = new MapView({
@@ -55,6 +91,20 @@ require([
   const kelpProductivityLayerToggle = document.getElementById("kelpProductivityLayer");
   kelpProductivityLayerToggle.addEventListener("change", function () {
     kelpProductivityLayer.visible = kelpProductivityLayerToggle.checked;
+  });
+
+  /*
+  // Toggle function of bathymetry layer
+  const bathymetryLayerToggle = document.getElementById("bathymetryLayer");
+  bathymetryLayerToggle.addEventListener("change", function () {
+    bathymetryLayer.visible = bathymetryLayerToggle.checked;
+  });
+  */
+
+  // Toggle function of federal and state waters layer
+  const federalAndStateWatersLayerToggle = document.getElementById("federalAndStateWatersLayer");
+  federalAndStateWatersLayerToggle.addEventListener("change", function () {
+    federalAndStateWatersLayer.visible = federalAndStateWatersLayerToggle.checked;
   });
 
   // Toggle function of shipping lanes layer
@@ -82,6 +132,14 @@ require([
         {
           layer: kelpProductivityLayer,
           title: "Kelp Productivity"
+        },
+        /*{
+          layer: bathymetryLayer,
+          title: "Bathymetry"
+        },*/
+        {
+          layer: federalAndStateWatersLayer,
+          title: "Federal and State Waters"
         },
         {
           layer: shippingLanesLayer,
