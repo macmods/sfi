4e458d419b1dba46873e7b6c54e631297c6cae8f
Danny7226 <dchai2@uci.edu>
2020-10-27 12:52:56 -0700

    revised function naming

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,22 +2,22 @@
 // including the buttons for toggling on and off each feature
 
 
-function navButton(a) {
-    if (a == "layer-toggle") {
+function navButton(selectedButton) {
+    if (selectedButton == "layer-toggle") {
         var x = document.getElementById("layerToggle"); //layerToggle is the id of widget 
         if (x.style.display === "none") {
           x.style.display = "block";
         } else {
           x.style.display = "none";
         }
-    } else if (a == "sfi-calc") {
+    } else if (selectedButton == "sfi-calc") {
         var x = document.getElementById("sfiCalc"); // TODO: Need to implement feature and name widget id to sfiCalc
         if (x.style.display === "none") {
           x.style.display = "block";
         } else {
           x.style.display = "none";
         }
-    } else if (a == "report-sfi") {
+    } else if (selectedButton == "report-sfi") {
         var x = document.getElementById("reportSfi"); // TODO: Need to implement feature and name widget id to reportSfi
         if (x.style.display === "none") {
           x.style.display = "block";
