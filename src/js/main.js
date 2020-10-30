621b1011c164e6c69fdd24b1ebdf74f1f824bef2
oscarli9 <guowel2@uci.edu>
2020-10-30 08:43:54 +0800

    Added principle ports layer and implemented the premitive data popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-//import { kelpProductivityLayerUrl, shippingLanesLayerUrl, dangerZonesAndRestrictedAreasLayerUrl, mpaInventoryLayerUrl } from './config.js';
+// import { kelpProductivityLayerUrl, shippingLanesLayerUrl, dangerZonesAndRestrictedAreasLayerUrl, mpaInventoryLayerUrl } from './config.js';
 
 require([
   // mapping
@@ -9,6 +9,7 @@ require([
   "esri/widgets/Legend",
   "esri/widgets/Search",
   "esri/widgets/Expand",
+  "esri/tasks/Locator",
   "esri/widgets/Bookmarks",
   "esri/widgets/Slider",
 ], function (
@@ -20,6 +21,7 @@ require([
   Legend,
   Search,
   Expand,
+  Locator,
   Bookmarks,
   Slider
 ) {
@@ -86,6 +88,291 @@ require([
     ],
   };
 
+  // Popup template for kelp productivity layer
+  var kelpProductivityPopupTemplate = {
+    // autocasts as new PopupTemplate()
+    title: "Kelp Productivity",
+    content: [
+      {
+        type: "fields",
+        fieldInfos: [
+          {
+            fieldName: "biomass",
+            label: "Biomass",
+          },
+        ],
+      },
+    ],
+  };
+
+  // Popup template for federal and state waters layer
+  var federalAndStateWatersPopupTemplate = {
+    // autocasts as new PopupTemplate()
+    title: "FederalAndStateWaters: {Jurisdicti}",
+    content: [
+      {
+        type: "fields",
+        fieldInfos: [
+          {
+            fieldName: "Jurisdicti",
+            label: "Jurisdiction",
+          },
+          {
+            fieldName: "area_mi2",
+            label: "Area (square miles)",
+          },
+          {
+            fieldName: "area_km2",
+            label: "Area (square kilometers)",
+          },
+          {
+            fieldName: "area_nm2",
+            label: "Area (square nautical miles)",
+          },
+          {
+            fieldName: "Shape__Area",
+          },
+          {
+            fieldName: "Shape__Length",
+          },
+        ],
+      },
+    ],
+  };
+
+  // Popup template for shipping lanes layer
+  var shippingLanesPopupTemplate = {
+    // autocasts as new PopupTemplate()
+    title: "ShippingLanes_SCA: {THEMELAYER}",
+    content: [
+      {
+        type: "fields",
+        fieldInfos: [
+          {
+            fieldName: "OBJL",
+          },
+          {
+            fieldName: "THEMELAYER",
+          },
+          {
+            fieldName: "INFORM",
+          },
+          {
+            fieldName: "OBJNAM",
+          },
+          {
+            fieldName: "Shape_Leng",
+          },
+          {
+            fieldName: "Shape_Area",
+          },
+          {
+            fieldName: "Shape__Area",
+          },
+          {
+            fieldName: "Shape__Length",
+          },
+        ],
+      },
+    ],
+  };
+
+  // Popup template for danger zones and restricted areas layer
+  var dangerZonesAndRestrictedAreasPopupTemplate = {
+    // autocasts as new PopupTemplate()
+    title: "DangerZonesAndRestrictedAreas_SCA",
+    content: [
+      {
+        type: "fields",
+        fieldInfos: [
+          {
+            fieldName: "effectiveD",
+          },
+          {
+            fieldName: "boundaryId",
+          },
+          {
+            fieldName: "boundaryNa",
+          },
+          {
+            fieldName: "boundaryTy",
+          },
+          {
+            fieldName: "boundaryDe",
+          },
+          {
+            fieldName: "instanceCo",
+          },
+          {
+            fieldName: "boundarySo",
+          },
+          {
+            fieldName: "boundary_1",
+          },
+          {
+            fieldName: "agencyOfUs",
+          },
+          {
+            fieldName: "contact",
+          },
+          {
+            fieldName: "nativeDatu",
+          },
+          {
+            fieldName: "state",
+          },
+          {
+            fieldName: "Shape_Leng",
+          },
+          {
+            fieldName: "Shape_Area",
+          },
+          {
+            fieldName: "Shape__Area",
+          },
+          {
+            fieldName: "Shape__Length",
+          },
+        ],
+      },
+    ],
+  };
+
+  // Popup template for MPA inventory layer
+  var mpaInventoryPopupTeamplate = {
+    // autocasts as new PopupTemplate()
+    title: "MPA Inventory: {Site_ID}",
+    content: [
+      {
+        type: "fields",
+        fieldInfos: [
+          {
+            fieldName: "Site_ID",
+          },
+          {
+            fieldName: "Area_KM_To",
+          },
+          {
+            fieldName: "Date_GIS_U",
+          },
+          {
+            fieldName: "Shape_Leng",
+          },
+          {
+            fieldName: "Site_Name",
+          },
+          {
+            fieldName: "Site_Label",
+          },
+          {
+            fieldName: "Gov_Level",
+          },
+          {
+            fieldName: "State",
+          },
+          {
+            fieldName: "NS_Full",
+          },
+          {
+            fieldName: "Prot_Lvl",
+          },
+          {
+            fieldName: "Mgmt_Plan",
+          },
+          {
+            fieldName: "Mgmt_Agen",
+          },
+          {
+            fieldName: "Fish_Rstr",
+          },
+          {
+            fieldName: "Pri_Con_Fo",
+          },
+          {
+            fieldName: "Cons_Focus",
+          },
+          {
+            fieldName: "Prot_Focus",
+          },
+          {
+            fieldName: "Permanence",
+          },
+          {
+            fieldName: "Constancy",
+          },
+          {
+            fieldName: "Estab_Yr",
+          },
+          {
+            fieldName: "URL",
+          },
+          {
+            fieldName: "Vessel",
+          },
+          {
+            fieldName: "Anchor",
+          },
+          {
+            fieldName: "Area_KM_Ma",
+          },
+          {
+            fieldName: "Shape_Le_1",
+          },
+          {
+            fieldName: "Shape_Area",
+          },
+          {
+            fieldName: "Shape__Area",
+          },
+          {
+            fieldName: "Shape__Length",
+          },
+        ],
+      },
+    ],
+  };
+
+  // Popup template for principal ports layer
+  var principalPortsPopupTemplate = {
+    // autocasts as new PopupTemplate()
+    title: "PrincipalPorts_SCA: {portName}",
+    content: [
+      {
+        type: "fields",
+        fieldInfos: [
+          {
+            fieldName: "portNumber",
+            label: "Port Number",
+          },
+          {
+            fieldName: "portName",
+            label: "Port Name",
+          },
+          {
+            fieldName: "totalCommo",
+            label: "Total Common",
+          },
+          {
+            fieldName: "domesticCo",
+            label: "Domestic Common",
+          },
+          {
+            fieldName: "foreignCom",
+            label: "Foreign Common",
+          },
+          {
+            fieldName: "importComm",
+            label: "Import Common",
+          },
+          {
+            fieldName: "exportComm",
+            label: "Export Common",
+          },
+        ],
+      },
+    ],
+  };
+
   // Kelp productivity layer
   const kelpProductivityLayer = new FeatureLayer({
     url:
@@ -93,6 +380,7 @@ require([
     visible: false,
     renderer: kelpProductivityRenderer,
     minScale: 9000000, // map scale at which layer becomes invisible
+    popupTemplate: kelpProductivityPopupTemplate,
   });
 
   /*
@@ -108,6 +396,7 @@ require([
     url:
       "https://services7.arcgis.com/4c8njmg1eMIbzYXM/ArcGIS/rest/services/FederalAndStateWaters/FeatureServer/0",
     visible: false,
+    popupTemplate: federalAndStateWatersPopupTemplate,
   });
 
   // Shipping lanes layer
@@ -116,6 +405,7 @@ require([
       "https://services7.arcgis.com/4c8njmg1eMIbzYXM/arcgis/rest/services/ShippingLanes_SCA/FeatureServer/1",
     visible: false,
     definitionExpression: "(OBJECTID < 3 OR " + "OBJECTID > 4)",
+    popupTemplate: shippingLanesPopupTemplate,
   });
 
   // Danger zones and restricted areas layer
@@ -123,6 +413,7 @@ require([
     url:
       "https://services7.arcgis.com/4c8njmg1eMIbzYXM/arcgis/rest/services/DangerZonesAndRestrictedAreas_SCA/FeatureServer/0",
     visible: false,
+    popupTemplate: dangerZonesAndRestrictedAreasPopupTemplate,
   });
 
   // MPA inventory layer
@@ -136,6 +427,20 @@ require([
       "OBJECTID < 54 OR " +
       "OBJECTID > 54 AND " +
       "OBJECTID < 94)",
+    popupTemplate: mpaInventoryPopupTeamplate,
+  });
+
+  // Principal ports layer
+  const principalPortsLayer = new FeatureLayer({
+    url:
+      "https://services7.arcgis.com/4c8njmg1eMIbzYXM/ArcGIS/rest/services/PrincipalPorts_SCA/FeatureServer/0",
+    visible: false,
+    popupTemplate: principalPortsPopupTemplate,
+  });
+
+  // Create a locator task using the world geocoding service
+  const locatorTask = new Locator({
+    url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer",
   });
 
   const webmap = new WebMap({
@@ -149,6 +454,7 @@ require([
       shippingLanesLayer,
       dangerZonesAndRestrictedAreasLayer,
       mpaInventoryLayer,
+      principalPortsLayer,
     ],
   });
 
@@ -214,6 +520,14 @@ require([
     mpaInventoryLayer.visible = mpaInventoryLayerToggle.checked;
   });
 
+  // Toggle function of principal ports layer
+  const principalPortsLayerToggle = document.getElementById(
+    "principalPortsLayer"
+  );
+  principalPortsLayerToggle.addEventListener("change", function () {
+    principalPortsLayer.visible = principalPortsLayerToggle.checked;
+  });
+
   /****************************************************
    * Define the UI
    ****************************************************/
@@ -247,6 +561,10 @@ require([
             layer: mpaInventoryLayer,
             title: "MPA Inventory",
           },
+          {
+            layer: principalPortsLayer,
+            title: "Principal Ports",
+          },
         ],
       }),
       view: view,
@@ -255,7 +573,7 @@ require([
     view.ui.add(legend, "top-left");
   });
 
-  // widget #2: Search
+  // Widget #2: Search
   const searchWidget = new Search({ view });
   view.ui.add(searchWidget, "top-right");
 
@@ -271,8 +589,8 @@ require([
     expanded: false,
   });
   view.ui.add(bkExpand, "top-left");
-  /***  User Input SFI feature   ***/
 
+  /***  User Input SFI feature   ***/
   const minOCDepthSlider = new Slider({
     container: "minOCDepthSlider",
     min: 10,
@@ -313,8 +631,8 @@ require([
     container: "weightingFactor",
     min: 0,
     max: 1,
-    steps: 1,
-    values: [0],
+    steps: 0.01,
+    values: [0.5],
     visibleElements: {
       labels: true,
       rangeLabels: true,
