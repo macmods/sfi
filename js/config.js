d97233d4a8a8efedc3cee930d14b9e2047bee888
Danny7226 <dchai2@uci.edu>
2020-11-30 11:17:46 -0800

    change url from NOAA hosted to Christina hosted

--- js/config.js
+++ js/config.js
@@ -3,15 +3,15 @@ const kelpProductivityLayerUrl =
 const bathymetryLayerUrl =
   "https://coast.noaa.gov/arcgis/rest/services/OceanReportingTool/BathymetricContours/MapServer/0";
 const shippingLanesLayerUrl =
-  "https://encdirect.noaa.gov/arcgis/rest/services/NavigationChartData/MarineTransportation/MapServer/0";
+  "https://services2.arcgis.com/zzN1kKcv4jyJtkCg/arcgis/rest/services/shippinglanes/FeatureServer/0";
 const dangerZonesAndRestrictedAreasLayerUrl =
-  "https://coast.noaa.gov/arcgis/rest/services/OceanReportingTool/DangerZonesAndRestrictedAreas/MapServer/0";
+  "https://services2.arcgis.com/zzN1kKcv4jyJtkCg/arcgis/rest/services/DangerZonesAndRestrictedAreas/FeatureServer/0";
 const mpaInventoryLayerUrl =
-  "https://services2.arcgis.com/C8EMgrsFcRFL6LrL/ArcGIS/rest/services/MPAI_2020_WMS/FeatureServer/0";
+  "https://services2.arcgis.com/zzN1kKcv4jyJtkCg/arcgis/rest/services/ProtectedAreas/FeatureServer/0";
 const principalPortsLayerUrl =
   "https://services2.arcgis.com/zzN1kKcv4jyJtkCg/arcgis/rest/services/List_of_Ports/FeatureServer/0";
 const federalAndStateWatersLayerUrl =
-  "https://coast.noaa.gov/arcgis/rest/services/OceanReportingTool/FederalAndStateWaters/MapServer/0";
+  "https://services2.arcgis.com/zzN1kKcv4jyJtkCg/arcgis/rest/services/FederalAndStateWaters/FeatureServer/0";
 
 const locatorTaskUrl =
   "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
